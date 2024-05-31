c7b5f68e23ff9032dbe785c3ff9a62c0700ff41e
Gabriel Guerra <gfranklings@gmail.com>
2024-05-31 08:36:49 -0400

    feat: adding nix support

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ stdenv, lib, pkgs ? import <nixpkgs> {}, ... }: pkgs.rustPlatform.buildRustPackage {
+  pname = "webx";
+  version = "git";
+
+  src = "${pkgs.webx-src}/napture";
+
+  nativeBuildInputs = lib.optionals stdenv.isLinux (with pkgs; [
+    pkg-config
+  ]);
+
+  propagatedBuildInputs = lib.optionals stdenv.isLinux (with pkgs; [
+    glib
+    pango
+    gdk-pixbuf
+    openssl_3_3
+    graphene
+    gtk4
+    libadwaita
+    lua5_4_compat
+  ]);
+
+  cargoLock = {
+    lockFile = "${pkgs.webx-src}/napture/Cargo.lock";
+  };
+
+  postInstall = ''
+    mkdir -p $out/share/metainfo $out/share/applications $out/share/icons
+    install -Dm644 $src/io.github.face_hh.Napture.metainfo.xml -t $out/share/metainfo/
+    install -Dm644 $src/io.github.face_hh.Napture.desktop -t $out/share/applications/
+    install -Dm644 $src/io.github.face_hh.Napture.svg -t $out/share/icons/hicolor/scalable/apps/
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    webx-src = {
+      url = "github:face-hh/webx";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @inputs: flake-utils.lib.eachDefaultSystem(system: let
+    pkgs = import nixpkgs {inherit system overlays;};
+    package = pkgs.callPackage ./default.nix {};
+
+    overlays = [
+      (_: _: {
+        inherit (inputs) webx-src;
+      })
+    ];
+  in {
+    packages.default = package;
+
+    devShells.default = let inherit (pkgs) mkShell; in mkShell {
+      name = "dev";
+      nativeBuildInputs = [pkgs.pkg-config];
+      buildInputs = with pkgs; [package cargo rustc];
+
+      propagatedBuildInputs = with pkgs; [
+        glib
+        pango
+        gdk-pixbuf
+        openssl_3_3
+        graphene
+        gtk4
+        libadwaita
+        lua5_4_compat
+      ];
+    };
+  });
+}
