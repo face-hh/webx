c36ff2f7516ed338d26c1b9de62af0112e8b7cb6
Xanderplayz16 <74632800+Xanderplayz16@users.noreply.github.com>
2024-06-02 00:24:02 -0400

    Add cryptographically secure random number generator to the generateApiKey function

--- dns/utils.js
+++ dns/utils.js
@@ -1,9 +1,11 @@
+const crypto = require('crypto');
+
 function generateApiKey(length) {
     const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let key = '';
 
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
+        const randomIndex = Math.floor(crypto.randomBytes(4).readUInt32LE(0)/0xffffffff * charset.length);
         key += charset.charAt(randomIndex);
     }
 
