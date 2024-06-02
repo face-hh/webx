72279f39c7750f498e6b10c29e407ac5a0de3aec
Mirek <115884310+m1reeek@users.noreply.github.com>
2024-06-02 17:03:01 +0200

    added .pl domain

--- dns/index.js
+++ dns/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 
 const TLD = [
     "mf", "btw", "fr", "yap", "dev", "scam", "zip", "root", "web", "rizz", "habibi", "sigma",
-    "now", "it", "soy", "lol", "uwu", "ohio"
+    "now", "it", "soy", "lol", "uwu", "ohio", "pl"
 ];
 
 
