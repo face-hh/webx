815d53b8b3f158521168d1f714e0046448153f03
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-03 14:19:41 +0200

    Install Rust using apt

--- napture/scripts/deps-install.sh
+++ napture/scripts/deps-install.sh
@@ -8,8 +8,7 @@ if [ -f /etc/os-release ]; then
     if [[ $PRETTY_NAME == "Ubuntu 24.04 LTS" ]]; then
         echo  "Installing dependencies..."
         sudo apt-get update
-        sudo apt-get install -y curl git build-essential libssl-dev libglib2.0-dev libcairo2-dev libgraphene-1.0-dev libgtk-4-dev libadwaita-1-dev liblua5.4-dev
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        sudo apt-get install -y curl git build-essential cargo libssl-dev libglib2.0-dev libcairo2-dev libgraphene-1.0-dev libgtk-4-dev libadwaita-1-dev liblua5.4-dev
         echo "Dependencies installed."
     else
         echo "Could not detect the Linux distribution and version. Now continuing with build, without attempting to install dependencies."
