b63f02f0a29f109e07fcff8d7dd27c9f7394b29d
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-03 13:35:13 +0200

    Offload the git status check to an external script

--- napture/install-linux.sh
+++ napture/install-linux.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Building Napture..."
-
+./scripts/git-status-check.sh
 ./scripts/deps-install.sh
 
-status=$(git status)
-
-# Check if the status contains the phrase "Your branch is behind"
-if [[ $status == *"Your branch is behind"* ]]; then
-    echo "Your branch is behind the remote repository."
-    echo "Pulling the latest changes..."
-    git pull origin $(git rev-parse --abbrev-ref HEAD)
-elif [[ $status == *"Your branch is up to date"* ]]; then
-    echo "Your branch is up to date with the remote repository."
-else
-    echo "Failed to determine the repository status."
-fi
-
+echo "Building Napture..."
 
 # Build Napture
 cargo build --release || exit 1
